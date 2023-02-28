9ed969a68cd30f9dd7f51506d3e1a59af1723cd1
nowsilver <46918839+rosa2070@users.noreply.github.com>
2023-02-28 18:25:43 +0900

    Add files via upload

---
+++ 강의/first.R
@@ -0,0 +1,169 @@
+ 
+
+1+1
+
+1/3
+
+2^3
+sqrt(4)
+
+install.packages("dplyr")
+library(dplyr)
+
+# 워킹 디렉토리
+
+변수 changable number
+variable
+
+a = 1
+print(a)
+
+b = "Hello"
+print(b)
+
+a = 2
+print(a)
+
+a = c(1,2)
+print(a)
+
+a = 1
+b = 2
+a = a + b
+print(a)
+
+a = 1
+
+a = 1
+a = a + 1
+
+a <- 1
+b <- 2
+a + b -> a
+print(a)
+
+var1 <- c(1,2,3,4,       5)
+var1
+
+var2 <- c(1:10)
+var2
+
+var3 <- seq(1, 10, 2)
+var3
+
+var3 <- seq(from-=1, to=10, by=2)
+var3
+
+help(seq)
+
+
+
+help(seq)
+
+
+str1 <- "Hello"
+# c() 함수는 변수에 여러 개의 값을 넣을 수 있다.
+str2 <- c("Hello", "Good Morning")
+str2
+
+a <- c(1, "Hello")
+a
+
+
+mode(c)
+
+mode(str1)
+typeof(str1)
+
+mode(a)
+c
+
+str1
+
+a
+
+# 1부터 10까지 합을 구해 봅시다
+
+sum(1:10)
+
+seq1 <- seq(1, 10, 1)
+sum1 <- sum(seq1)
+print(sum1)
+
+# 1부터 100까지 모든 홀수들의 평균은?
+mean(seq(1, 100, 2))
+
+var(seq(1, 100, 2))
+
+sqrt(var(seq(1, 100, 2)))
+sd(seq(1, 100, 2))
+
+max(seq(1, 100, 2))
+
+installed.packages()
+install.packages("stringr")
+
+paste()
+str2
+paste("Hello", "Good")
+paste0("Hello", "Good")
+
+str <- c("Hello",  "world", "is", "good")
+str
+paste(str, collapse = " ")
+sentence <- paste(str, collapse = " ")
+
+package library  
+
+
+qplot()
+
+ggplot()
+
+library(ggplot2)
+x <- c("a", "a", 'b', 'c')
+
+ggplot(data = diamonds, aes(x=carat, y=price, color=cut)) +
+  geom_point()
+
+help(mean)
+help(ggplot)
+?ggplot
+?mean
+
+1. 변수이름은 scores
+scores <- c(80, 60, 70, 50, 90)
+scores
+
+avg <- mean(scores)
+avg
+
+3. 평균을 변수 avg 에 담아봅시다.
+
+a <- 1
+a <- c(1,2)
+
+= , <- : assignment operator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
